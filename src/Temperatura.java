3fc9b8c5640446379d5ee0c36fdee4280e44576a
gosiamm83 <machmalgorzata@gmail.com>
2018-06-16 16:57:19 +0200

    Wprowadzono przliczanie w obydwie strony C->K i K->C

--- src/Temperatura.java
+++ src/Temperatura.java
@@ -11,14 +11,12 @@ public class Temperatura extends JFrame implements ActionListener {
     private JTextField tCelsius, tFarenheit;
     private JButton bKonwetuj;
     private double tempCelsius, tempFareheit;
-    private JCheckBox chWielkie;
-    private ButtonGroup bgRozmiar;
-    private JRadioButton rbMaly, rbSredni, rbDuzy;
+    private ButtonGroup radioPanel;
+    private JRadioButton rbFtoC, rbCtoF;
 
     public Temperatura() {
 
-
-        setSize(400, 220);
+        setSize(400, 320);
         setTitle("Obliczenie temperatury Celsius to Farenheit");
         setLayout(null);
 
@@ -30,7 +28,7 @@ public class Temperatura extends JFrame implements ActionListener {
         tCelsius.setBounds(170, 20, 100, 20);
         add(tCelsius);
         tCelsius.addActionListener(this);
-        tCelsius.setToolTipText("Podaj temeperaturę w stopniach Celsiusza");
+        //tCelsius.setToolTipText("Podaj temeperaturę w stopniach Celsiusza");
 
         lFarenheit = new JLabel(" Stopnie Farenheita: ");
         lFarenheit.setBounds(20, 70, 150, 20);
@@ -39,59 +37,52 @@ public class Temperatura extends JFrame implements ActionListener {
         tFarenheit = new JTextField();
         tFarenheit.setBounds(170, 70, 100, 20);
         add(tFarenheit);
+        tFarenheit.addActionListener(this);
+        //tFarenheit.setToolTipText("Podaj temeperaturę w stopniach Farenheita");
 
         bKonwetuj = new JButton("Przelicz");
         bKonwetuj.setBounds(120, 120, 150, 20);
-        bKonwetuj.addActionListener(this);
         add(bKonwetuj);
+        bKonwetuj.addActionListener(this);
 
+        radioPanel = new ButtonGroup();
 
-        chWielkie = new JCheckBox("Wielkie litery");
-        chWielkie.setBounds(280, 120, 150, 20);
-        add(chWielkie);
-        //chWielkie.addActionListener(this);
+        rbCtoF = new JRadioButton("Celsius to Farenheit");
+        rbCtoF.setBounds(50, 250, 150, 20);
+        rbCtoF.setSelected(true);
+        radioPanel.add(rbCtoF);
+        add(rbCtoF);
 
-        bgRozmiar = new ButtonGroup();
-        rbMaly = new JRadioButton("Mały", true);
-        rbMaly.setBounds(50, 150, 100, 20);
-        bgRozmiar.add(rbMaly);
-        add(rbMaly);
-        rbMaly.addActionListener(this);
-
-        rbSredni = new JRadioButton("Sredni", false);
-        rbSredni.setBounds(150, 150, 100, 20);
-        bgRozmiar.add(rbSredni);
-        add(rbSredni);
-        rbSredni.addActionListener(this);
-
-        rbDuzy = new JRadioButton("Duzy", false);
-        rbDuzy.setBounds(250, 150, 100, 20);
-        bgRozmiar.add(rbDuzy);
-        add(rbDuzy);
-        rbDuzy.addActionListener(this);
-
+        rbFtoC = new JRadioButton("Farenheir to Celsius");
+        rbFtoC.setBounds(200, 250, 150, 20);
+        radioPanel.add(rbFtoC);
+        add(rbFtoC);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         Object zrodlo = e.getSource();
 
-        if (zrodlo == bKonwetuj || zrodlo == tCelsius) {
-            if (chWielkie.isSelected()) {
-                tFarenheit.setFont(new Font("SansSerif", Font.BOLD, 18));
-            } else {
-                tFarenheit.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        if (zrodlo == bKonwetuj) {
+            if (rbCtoF.isSelected()) {
+                tempCelsius = Double.parseDouble(tCelsius.getText());
+                tempFareheit = 32.0 + (9.0 / 5.0) * tempCelsius;
+                tFarenheit.setText(String.valueOf(tempFareheit));
+            } else if (rbFtoC.isSelected()) {
+                tempFareheit = Double.parseDouble(tFarenheit.getText());
+                tempCelsius = (tempFareheit - 32.0) * (5.0 / 9.0);
+                tCelsius.setText(String.valueOf(tempCelsius));
             }
+        }
+        if (zrodlo == tCelsius) {
             tempCelsius = Double.parseDouble(tCelsius.getText());
             tempFareheit = 32.0 + (9.0 / 5.0) * tempCelsius;
             tFarenheit.setText(String.valueOf(tempFareheit));
-
-        } else if (zrodlo == rbMaly) {
-            tFarenheit.setFont(new Font("SansSerif", Font.PLAIN, 12));
-        } else if (zrodlo == rbSredni) {
-            tFarenheit.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        } else if (zrodlo == rbDuzy) {
-            tFarenheit.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        } else if (zrodlo == tFarenheit) {
+            tempFareheit = Double.parseDouble(tFarenheit.getText());
+            tempCelsius = (tempFareheit - 32.0) * (5.0 / 9.0);
+            tCelsius.setText(String.valueOf(tempCelsius));
         }
     }
 
@@ -99,8 +90,6 @@ public class Temperatura extends JFrame implements ActionListener {
         Temperatura temp = new Temperatura();
         temp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         temp.setVisible(true);
-
-
     }
 }
 
